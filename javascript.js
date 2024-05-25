fdf81ebc04c328fe0de220dc68032ac1f35c6dbc
Matheus Fernandes <matheus.oliveira147@hotmail.com.br>
2024-05-25 01:29:51 -0300

    atualização

--- javascript.js
+++ javascript.js
@@ -1,4 +1,21 @@
-let hora = Document.getElementByid('.hora')
+const horas = document.getElementByid('horas');
 
+const minutos = document.getElementByid('minutos');
+
+const segundos = document.getElementByid('segundos');
+
+const relogio = setInterval (function time(){
+    let dateToday = new Date();
+    let hor = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    horas.textContent = hor;
+    minutos.textContent = min;
+    segundos.textContent = s;
+
+
+
+})
 
 
